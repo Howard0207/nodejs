adb49febf8968f876202da5913e02ed8fe4376b6
Howard0207 <wode163_youjian@163.com>
2018-05-03 23:33:01 +0800

    add content-view function and views function

--- router/main.js
+++ router/main.js
@@ -3,20 +3,40 @@ let router = express.Router()
 // Category 模型
 let Category = require('../models/Category')
 let Content = require('../models/Content')
-router.get('/',function(req,res) {
-  let data = {
+
+let data = {}
+
+/**
+ * 处理通用的数据
+ */
+router.use((req,res,next) => {
+  data ={
     userInfo: req.userInfo,
-    categories: [],
-    count: 0,
-    page : Number(req.query.page || 1),// 实际要判断传递数据的类型是否是number，这里暂不处理
-    pages: 0,
-    limit: 10,
+    categories: []
   }
-  // 读取所有分类信息
+
   Category.find().then((categories) => {
     data.categories = categories
-    return Content.count()
-  }).then((count) => {
+    next()
+  })
+})
+
+
+
+router.get('/',function(req,res) {
+
+  data.category = req.query.category || ''
+  data.count = 0
+  data.page =  Number(req.query.page || 1)// 实际要判断传递数据的类型是否是number，这里暂不处理
+  data.pages = 0
+  data.limit = 3
+  let where = {}
+
+  if(data.category) {
+    where.category = data.category
+  }
+  // 读取所有分类信息
+  Content.where(where).count().then((count) => {
     data.count =  count
     // 计算总页数
     data.pages = Math.ceil(data.count/data.limit)
@@ -27,11 +47,23 @@ router.get('/',function(req,res) {
     
     let skip = (data.page-1)*data.limit
 
-    return Content.find().limit(data.limit).skip(skip).populate(['category','user']).sort({addTime: -1})
+    return Content.where(where).find().limit(data.limit).skip(skip).populate(['category','user']).sort({addTime: -1})
    
   }).then((contents) => {
     data.contents = contents
     res.render('main/index',data)
+  })
+})
+
+router.get('/view', (req,res) => {
+  let contentId = req.query.contentid || ''
+  Content.findOne({
+    _id: contentId
+  }).then((content) => {
+    data.content = content
+    content.views++
+    content.save()
+    res.render('main/view',data)
   })
 })
 
